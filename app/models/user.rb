14f103a4453ee33e92950853b2d69f449efc71df
arvind4501 <arvindjangirlpu@gmail.com>
2022-02-14 18:01:16 +0530

    validate sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
+  validates :dob, presence: true
+  validates :gender, presence: true
+
   has_many :albums, dependent: :destroy
 end
